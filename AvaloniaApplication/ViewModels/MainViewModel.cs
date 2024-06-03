0759fbbe65d8f3d695f9ff364da5b0f12ae82a54
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2024-06-02 17:38:09 -0700

    wip

--- AvaloniaApplication/ViewModels/MainViewModel.cs
+++ AvaloniaApplication/ViewModels/MainViewModel.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using Avalonia.Collections;
+using Avalonia.Threading;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 
 namespace AvaloniaApplication.ViewModels;
@@ -7,27 +11,48 @@ public partial class MainViewModel : ViewModelBase
 {
     public ObservableCollection<Person> People { get; }
 
+    [ObservableProperty]
+    private DataGridCollectionView _dataGridObjects;
+
+    private DispatcherTimer _disTimer = new DispatcherTimer();
+
     public MainViewModel()
     {
         var people = new List<Person>
         {
             new Person("Neil", "Armstrong"),
             new Person("Buzz", "Lightyear"),
-            new Person("James", "Kirk")
+            new Person("James", "Kirk"),
+            new Person("Fem", "Fem", false)
         };
+
         People = new ObservableCollection<Person>(people);
+
+        DataGridObjects = new DataGridCollectionView(People);
+
+        DataGridObjects.Filter = item => ((Person)item).Male == true;
+
+        _disTimer.Interval = TimeSpan.FromSeconds(1);
+        _disTimer.Tick += _disTimer_Tick; ;
+        _disTimer.Start();
+    }
+
+    private void _disTimer_Tick(object? sender, EventArgs e)
+    {
+        People.Add(new Person("test", "test", false));
     }
 }
-
 
 public class Person
 {
     public string FirstName { get; set; }
     public string LastName { get; set; }
+    public bool Male { get; set; } = true;
 
-    public Person(string firstName, string lastName)
+    public Person(string firstName, string lastName, bool male = true)
     {
         FirstName = firstName;
         LastName = lastName;
+        Male = male;
     }
 }
