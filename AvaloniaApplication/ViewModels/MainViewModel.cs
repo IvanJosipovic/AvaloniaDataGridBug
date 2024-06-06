3fac2f3752a672598aa9d341cedfc478f0e1ce3f
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2024-06-05 21:33:53 -0700

    wip

--- AvaloniaApplication/ViewModels/MainViewModel.cs
+++ AvaloniaApplication/ViewModels/MainViewModel.cs
@@ -1,15 +1,19 @@
 ﻿using Avalonia.Collections;
 using Avalonia.Threading;
 using CommunityToolkit.Mvvm.ComponentModel;
+using Swordfish.NET.Collections;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace AvaloniaApplication.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
     public ObservableCollection<Person> People { get; }
+
+    public ConcurrentObservableDictionary<string, Person> People2 { get; } = [];
 
     [ObservableProperty]
     private DataGridCollectionView _dataGridObjects;
@@ -28,9 +32,17 @@ public partial class MainViewModel : ViewModelBase
 
         People = new ObservableCollection<Person>(people);
 
-        DataGridObjects = new DataGridCollectionView(People);
+        People2 = new ConcurrentObservableDictionary<string, Person>()
+        {
+            new KeyValuePair<string, Person>("pp1", new Person("pp1", "pp1")),
+            new KeyValuePair<string, Person>("pp2", new Person("pp2", "pp1")),
+            new KeyValuePair<string, Person>("pp3", new Person("pp3", "pp1")),
+            new KeyValuePair<string, Person>("pp4", new Person("pp4", "pp1", false)),
+        };
 
-        DataGridObjects.Filter = item => ((Person)item).Male == true;
+        DataGridObjects = new DataGridCollectionView(People2);
+
+        DataGridObjects.Filter = item => ((KeyValuePair<string, Person>)item).Value.Male == false;
 
         _disTimer.Interval = TimeSpan.FromSeconds(1);
         _disTimer.Tick += _disTimer_Tick; ;
@@ -39,7 +51,21 @@ public partial class MainViewModel : ViewModelBase
 
     private void _disTimer_Tick(object? sender, EventArgs e)
     {
-        People.Add(new Person("test", "test", false));
+        var rand = RandomString(5);
+
+        People2.Add(new KeyValuePair<string, Person>(rand, new Person(rand, rand, true)));
+
+        //People2.RemoveAt(0);
+    }
+
+
+    private static Random random = new Random();
+
+    public static string RandomString(int length)
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        return new string(Enumerable.Repeat(chars, length)
+            .Select(s => s[random.Next(s.Length)]).ToArray());
     }
 }
 
